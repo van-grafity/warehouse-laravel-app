9e00cb5e08a4c01ecc758803c49d9685432d7d95
Ridwan Pranata (GLA) <ridwanpranata10@gmail.com>
2024-03-28 14:53:02 +0700

    hiding content header

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -5,7 +5,7 @@
 @stop
 
 @section('content_header')
-    <div class="row mb-2">
+    <div class="row mb-2 d-none">
         <div class="col-sm-6">
             <h1 class="m-0">@yield('page_title')</h1>
         </div>
