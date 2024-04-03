da9c1cf2a7f51a0868d179ba8b283beb57cae60b
Ridwan Pranata (GLA) <ridwanpranata10@gmail.com>
2024-04-03 14:28:25 +0700

    fabric request sync single fbr, by their serial number

--- resources/views/pages/fabric-request/detail.blade.php
+++ resources/views/pages/fabric-request/detail.blade.php
@@ -16,7 +16,12 @@
                 </h3>
 
                 <div class="card-tools ml-auto p-3">
-                    <button type="button" class="btn btn-default mr-2" onclick="sync_fabric_request('{{$fabric_request->fbr_serial_number }}')"><i class="fas fa-sync-alt"></i> Syncronize Fabric Request</button>
+                    <button type="button" class="btn btn-default mr-2" 
+                        data-fbr-serial-number = "{{$fabric_request->fbr_serial_number }}"
+                        onclick="sync_fabric_request(this)">
+                        <i class="fas fa-sync-alt"></i> 
+                        Syncronize Fabric Request
+                    </button>
                     <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
                         <i class="fas fa-minus"></i>
                     </button>
@@ -96,9 +101,66 @@
 
 <script type="text/javascript">
     const token = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+    
+    const cutting_app_api_baseurl = "{{ config('api.cutting_app.base_url') }}";
+    const cutting_app_api_token = "{{ config('api.cutting_app.token') }}";
 
     // ## URL List
+    const sync_url = "{{ route('fabric-request.sync') }}";
 
+    const sync_fabric_request = async (element) => {
+
+        $(element).addClass('loading').attr('disabled',true);
+        
+        let fbr_serial_number = $(element).data('fbr-serial-number');
+        let fbr_sync_url = `${cutting_app_api_baseurl}/fabric-request-sync/get-fabric-request`;
+
+        let response = await using_axios({
+            url: fbr_sync_url,
+            method: 'GET',
+            data: {
+                fbr_serial_number : fbr_serial_number,
+            },
+            token : cutting_app_api_token,
+        });
+
+        if ([401,404].includes(response.status)) {
+            $(element).removeClass('loading').attr('disabled',false);
+            swal_failed({
+                title : response.message,
+            });
+            return false;
+        }
+
+        let fetch_data = {
+            url: sync_url,
+            method: "POST",
+            data: response.data,
+            token: token,
+        }
+
+        try {
+            let response_sync = await using_fetch(fetch_data);
+            $(element).removeClass('loading').attr('disabled',false);
+
+            if(response_sync.status != 'success') {
+                swal_failed({
+                    title : "Something went wrong",
+                });
+                return false;
+            }
+            
+            swal_info({'title': `${response_sync.message} (${fbr_serial_number})`, timer: 3000, reload_option: true });
+
+        } catch (error) {
+            $(element).removeClass('loading').attr('disabled',false);
+            swal_failed({
+                title : "Something went wrong",
+                text : "Try to decrease range of date filter",
+            });
+            return false;
+        }
+    }
 
 </script>
 @stop
