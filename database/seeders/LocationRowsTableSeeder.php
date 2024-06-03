3b36d8755ccd5a0fb84928d8b05218ba2b8ebbe6
Ridwan Pranata (GLA) <ridwanpranata10@gmail.com>
2024-06-03 15:21:30 +0700

    optimize code for location row seeder

--- database/seeders/LocationRowsTableSeeder.php
+++ database/seeders/LocationRowsTableSeeder.php
@@ -13,51 +13,13 @@ class LocationRowsTableSeeder extends Seeder
      */
     public function run(): void
     {
-        $data = [
-            [
-                'row' => 'A',
-                'description' => 'Row A',
-            ],
-            [
-                'row' => 'B',
-                'description' => 'Row B',
-            ],
-            [
-                'row' => 'C',
-                'description' => 'Row C',
-            ],
-            [
-                'row' => 'D',
-                'description' => 'Row D',
-            ],
-            [
-                'row' => 'E',
-                'description' => 'Row E',
-            ],
-            [
-                'row' => 'F',
-                'description' => 'Row F',
-            ],
-            [
-                'row' => 'G',
-                'description' => 'Row G',
-            ],
-            [
-                'row' => 'H',
-                'description' => 'Row H',
-            ],
-            [
-                'row' => 'I',
-                'description' => 'Row I',
-            ],
-            [
-                'row' => 'J',
-                'description' => 'Row J',
-            ],
-        ];
+        $rack_code = range('A', 'J');
 
-        foreach ($data as $key => $location) {
-            LocationRow::create($location);
+        foreach ($rack_code as $code) {
+            LocationRow::create([
+                'row' => $code,
+                'description' => 'Row ' . $code,
+            ]);
         }
     }
 }
