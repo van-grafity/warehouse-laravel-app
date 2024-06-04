273ef05715dd9ccffa24fc8b6b815966bbba0534
Ridwan Pranata (GLA) <ridwanpranata10@gmail.com>
2024-06-04 16:46:37 +0700

    change rack location title

--- resources/views/pages/rack-location/index.blade.php
+++ resources/views/pages/rack-location/index.blade.php
@@ -8,7 +8,7 @@
     <div class="col-12">
         <div class="card">
             <div class="card-header d-flex p-0">
-                <h3 class="card-title p-3 my-auto"> Rack Location List </h3>
+                <h3 class="card-title p-3 my-auto"> Rack Location </h3>
             </div>
             <!-- /.card-header -->
             <div class="card-body">
